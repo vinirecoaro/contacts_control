9072e7c6f014109e7eda4bb01e8648a05041282e
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-09-04 21:26:45 -0300

    Create DabaseContext class

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+using ContactsControl.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ContactsControl.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContactModel> Contacts {get; set;}
+    }
+}
